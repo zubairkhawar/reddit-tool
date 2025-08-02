208d36af6502ba3eb80c260092632ee812ad3c0b
zubairkhawar <zubairkhawer@gmail.com>
2025-08-02 21:18:40 +0500

    Configure AWS PostgreSQL database with environment variables and update env.example

---
+++ backend/reddit/settings.py
@@ -0,0 +1,16 @@
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='reddit'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+        'OPTIONS': {
+            'sslmode': config('DB_SSLMODE', default='require'),
+        },
+    }
+} 
